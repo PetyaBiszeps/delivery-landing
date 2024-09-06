06f6300862edecd5c3a7b446a949cfd13eb7240f
Yaroslav Petrov <PersonalYP00@gmail.com>
2024-09-06 04:12:33 +0300

    commit changes

--- src/js/animations.js
+++ src/js/animations.js
@@ -1,17 +1,3 @@
 export function animations() {
-    // Scroll Reveal API
-    ScrollReveal({
-        reset: true,
-        // distance: "50px",
-        duration: 800,
-        delay: 400
-    })
-
-    ScrollReveal().reveal(
-        '.home__container,' +
-        '.about__container,' +
-        '.security__container,' +
-        '.services h2, .services__container,' +
-        '.app__container,' +
-        '.contact__container');
+    
 }
